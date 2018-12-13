900ee80a77e8d9d1a4ca6b2678aadcfd46ae7aee
schoerkhuberLorenz <lschoerk@htl-steyr.ac.at>
2018-12-13 14:55:37 +0100

    Chat mit BadWordFilter, Chat mit Scrollbar

--- src/Chat.java
+++ src/Chat.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.*;
 
 public class Chat extends JFrame {
 
@@ -12,6 +13,7 @@ public class Chat extends JFrame {
     private JTextField input;
     private JButton butsend;
     private JTextArea chathistory;
+    private JScrollPane scpane;
     private ActionListener sendListener;
 
     public Chat(Game cb){
@@ -24,11 +26,28 @@ public class Chat extends JFrame {
         input = new JTextField();
         chathistory = new JTextArea();
         chathistory.setEditable(false);
+        scpane = new JScrollPane(chathistory,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         butsend = new JButton("SEND");
         butsend.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Message einfuegen
+                //BadwordFilter
+                try {
+                    String tmp = input.getText();
+                    FileReader fr = new FileReader(new File("badwords.txt"));
+                    BufferedReader br = new BufferedReader(fr);
+                    String stg;
+                    while ((stg = br.readLine()) !=  null){
+                        tmp = tmp.toUpperCase();
+                        if(tmp.contains(stg)){
+                            input.setText("no bad words");
+                        }
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
                 sendListener.actionPerformed(new ActionEvent(this, 0, "chat;" + input.getText()));
                 input.setText("");
             }
@@ -38,6 +57,7 @@ public class Chat extends JFrame {
             public void keyTyped(KeyEvent e) {
                 if(e.getKeyChar() == '\n'){
                     butsend.doClick();
+                    input.requestFocus();
                 }
             }
         });
@@ -55,7 +75,7 @@ public class Chat extends JFrame {
         subpanel.add(input, BorderLayout.CENTER);
         subpanel.add(butsend, BorderLayout.EAST);
         mainpanel.add(subpanel, BorderLayout.SOUTH);
-        mainpanel.add(chathistory, BorderLayout.CENTER);
+        mainpanel.add(scpane, BorderLayout.CENTER);
         this.add(mainpanel);
         this.setVisible(true);
 
