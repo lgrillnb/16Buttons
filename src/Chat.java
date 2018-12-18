1cd4f38c74693249f2b96d8187d6a793f20f8976
schoerkhuberLorenz <lschoerk@htl-steyr.ac.at>
2018-12-18 16:10:38 +0100

    Chat mit Vorschlägen, Chathistory bleibt immer ganz unten

--- src/Chat.java
+++ src/Chat.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,15 +11,21 @@ public class Chat extends JFrame {
 
     private JPanel mainpanel;
     private JPanel subpanel;
+    private JPanel suggespanel;
     private JTextField input;
     private JButton butsend;
+    private JButton butbusted;
+    private JButton butfail;
+    private JButton butlooser;
+    private JButton butdestroyed;
+    private JButton butknockout;
     private JTextArea chathistory;
     private JScrollPane scpane;
     private ActionListener sendListener;
 
     public Chat(Game cb){
         this.setTitle("Chat");
-        this.setSize(400, 400);
+        this.setSize(500, 400);
         this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         this.setLayout(new BorderLayout());
         this.setLocation(cb.getLocation().x + cb.getWidth() - 10, cb.getLocation().y);
@@ -27,6 +34,10 @@ public class Chat extends JFrame {
         chathistory = new JTextArea();
         chathistory.setEditable(false);
         scpane = new JScrollPane(chathistory,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        DefaultCaret caret = (DefaultCaret)chathistory.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
         butsend = new JButton("SEND");
         butsend.addActionListener(new ActionListener() {
             @Override
@@ -70,12 +81,59 @@ public class Chat extends JFrame {
             }
         });
 
+
         mainpanel = new JPanel(new BorderLayout());
         subpanel = new JPanel(new BorderLayout());
         subpanel.add(input, BorderLayout.CENTER);
         subpanel.add(butsend, BorderLayout.EAST);
         mainpanel.add(subpanel, BorderLayout.SOUTH);
         mainpanel.add(scpane, BorderLayout.CENTER);
+
+
+        suggespanel = new JPanel( new FlowLayout());
+        butbusted = new JButton("BUSTED");
+        butfail = new JButton("FAIL");
+        butlooser = new JButton("LOOSER");
+        butdestroyed = new JButton("DESTROYED");
+        butknockout = new JButton("KNOCK OUT");
+        suggespanel.add(butbusted);
+        suggespanel.add(butfail);
+        suggespanel.add(butlooser);
+        suggespanel.add(butdestroyed);
+        suggespanel.add(butknockout);
+        subpanel.add(suggespanel, BorderLayout.NORTH);
+
+        butbusted.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                sendListener.actionPerformed(new ActionEvent(this, 0, "chat;" + "BUSTED"));
+            }
+        });
+        butfail.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                sendListener.actionPerformed(new ActionEvent(this, 0, "chat;" + "FAIL"));
+            }
+        });
+        butlooser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                sendListener.actionPerformed(new ActionEvent(this, 0, "chat;" + "LOOSER"));
+            }
+        });
+        butdestroyed.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                sendListener.actionPerformed(new ActionEvent(this, 0, "chat;" + "I HAVE DESTROYED YOU"));
+            }
+        });
+        butknockout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                sendListener.actionPerformed(new ActionEvent(this, 0, "chat;" + "YOU GOT KNOCKED OUT"));
+            }
+        });
+
         this.add(mainpanel);
         this.setVisible(true);
 
